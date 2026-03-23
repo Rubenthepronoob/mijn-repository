ffcca789c78e7571043ab6bece5f7436523dfb94
Rubenthepronoob <ruben.de.jong2009@outlook.com>
2026-03-23 13:06:35 +0100

    finished creating layout left bar and added a new page

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,7 +1,5 @@
 var p = document.getElementById("p");
-
 function show() {
-  p.classList.toggle('hideP');
+p.classList.toggle('hideP');
 }
-
 document.getElementById('sources').addEventListener('click', show);
