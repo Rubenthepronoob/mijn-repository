3aedbc6896751c6285271a77e022f625e42c8397
Rubenthepronoob <ruben.de.jong2009@outlook.com>
2026-03-25 15:22:18 +0100

    i changed the left bar

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,5 +1,25 @@
-var p = document.getElementById("p");
+var p1 = document.getElementById("p1");
 function show() {
-p.classList.toggle('hideP');
+p1.classList.toggle('hideP1');
+}
+document.getElementById('gamesonreddit').addEventListener('click', show);
+var p2 = document.getElementById("p2");
+function show() {
+p2.classList.toggle('hideP2');
+}
+document.getElementById('customfeeds').addEventListener('click', show);
+var p3 = document.getElementById("p3");
+function show() {
+p3.classList.toggle('hideP3');
+}
+document.getElementById('recent').addEventListener('click', show);
+var p4 = document.getElementById("p4");
+function show() {
+p4.classList.toggle('hideP4');
+}
+document.getElementById('communitys').addEventListener('click', show);
+var p5 = document.getElementById("p5");
+function show() {
+p5.classList.toggle('hideP5');
 }
 document.getElementById('sources').addEventListener('click', show);
